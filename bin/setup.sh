3dce005ba1c8de58d9760a29cc2c5334cc7a7ef7
davidtan <davidtan@thoughtworks.com>
2017-11-10 00:29:24 +0800

    Annotate setup script with description

--- bin/setup.sh
+++ bin/setup.sh
@@ -2,6 +2,7 @@
 
 set -e
 
+# if homebrew is not installed, install homebrew
 if [[ `which brew` ]]; then
   echo "OK Found homebrew!"
 else
@@ -9,6 +10,7 @@ else
   ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
 fi
 
+# if python3 is not installed, install python3 with homebrew
 if [[ `which python3` ]]; then
   echo "OK Found python3!"
 else
@@ -16,7 +18,9 @@ else
   brew install python3
 fi
 
-if [ ! -d .venv ]; then
+# create virtual environment (which is just a folder in your current project directory). '.venv' is just a name - you can name it anything you like (e.g. 'my_awesome_virtual_env'. The period in .venv is just a convention to denote it has a hidden folder that we should not commit to git.
+# the if condition skips this step if folder .venv already exists
+if [ ! -d .venv ]; then #
   echo "INFO: Creating virtual env folder in current directory"
   python3 -m venv .venv
 fi
@@ -24,7 +28,7 @@ fi
 echo "Activating virtual environment"
 source .venv/bin/activate
 
-echo "Installing python libraries"
+echo "Installing python libraries in the virtual environment directory"
 pip install -r requirements.txt
 
 echo "Adding virtual environment to jupyter notebook"

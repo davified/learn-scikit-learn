4fe1ee3ea15f022b79e0f4d9ab39b4703817e5aa
davidtan <davidtan@thoughtworks.com>
2018-05-17 07:29:09 +0800

    Update setup script to avoid downloading file if file already exists

--- bin/setup.sh
+++ bin/setup.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-if [[ $OSTYPE == "darwin"* ]]; then
+if [[ $OSTYPE != "darwin"* ]]; then
   echo "[INFO] Non-Mac OSX operating system detected"
   echo "[TODO] Open http://continuum.io/downloads with your web browser"
   echo "[TODO] Download the Python 3 installer for your OS"
@@ -14,9 +14,12 @@ fi
 if [[ `which conda` ]]; then
   echo "[INFO] OK Found conda!"
 else
-  echo "[INFO] Downloading anaconda installation script..."
-  echo "[INFO] This is a 511MB file and will some time to complete..."
-  curl https://repo.anaconda.com/archive/Anaconda3-5.1.0-MacOSX-x86_64.sh -o ./anaconda3.sh
+  if [[ ! -f ./anaconda3.sh  ]]; then
+    echo "[INFO] Downloading anaconda installation script..."
+    echo "[INFO] This is a 511MB file and will some time to complete..."
+    echo "downloading 511mb file"
+    # curl https://repo.anaconda.com/archive/Anaconda3-5.1.0-MacOSX-x86_64.sh -o ./anaconda3.sh
+  fi
 
   echo "[INFO] Running anaconda installation script..."
   bash anaconda3.sh -b -p ~/anaconda3
